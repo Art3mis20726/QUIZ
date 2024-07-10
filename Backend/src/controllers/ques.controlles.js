4b37f8f5cdd8a428e6942971152ab5ea399ba766
Art3mis20726 <gitpats@gmail.com>
2024-07-10 18:43:03 +0530

    "Added cloudinary upload functionality, implemented question upload with multer, and created routes for posting questions, getting random questions, and checking answers."

--- Backend/src/controllers/ques.controlles.js
+++ Backend/src/controllers/ques.controlles.js
@@ -5,16 +5,18 @@ import { User } from "../models/user.models.js";
 import ApiError from "../utils/apiError.utils.js";
 import asyncHandler from "../utils/asyncHandler.utils.js";
 import { ApiResponse } from "../utils/apiResponse.utils.js";
+import { uploadoncloudinary } from "../utils/cloudinary.utils.js";
 
 const quesUpload = asyncHandler(async (req, res) => {
-  const { type, question, opt, correct } = req.body;
+  const { type, opt, correct } = req.body;
+  const quesLocalPath=req.files?.ques[0].path
 
   // Validate required fields
   if (!type) {
     throw new ApiError(400, "Type is not defined");
   }
-  if (!question) {
-    throw new ApiError(400, "Question is not defined");
+  if (!quesLocalPath) {
+    throw new ApiError(400, "Question local path not defined");
   }
   if (!opt || opt.length !== 4) {
     throw new ApiError(400, "Options should be an array of 4 elements");
@@ -22,24 +24,27 @@ const quesUpload = asyncHandler(async (req, res) => {
   if (!correct) {
     throw new ApiError(400, "Correct answer is not defined");
   }
-
+  const question=await uploadoncloudinary(quesLocalPath)
+  if(!question.url){
+    throw new ApiError(400, "Question not uploaded")
+  }
+   const quesUrl=question.url
+  console.log(quesUrl);
   // Find the user
   const user = await User.findById(req.user?._id);
   if (!user) {
     throw new ApiError(400, "User not found");
   }
-
-  // Check if there is already a type of question for the user
+   // Check if there is already a type of question for the user
   const typeOfQuestion = await Questions.findOne({ type, owner: user._id });
 
-  if (!typeOfQuestion) {
-    // Create a new type of question if it doesn't exist
+  if (!typeOfQuestion) {     // Create a new type of question if it doesn't exist
     const newQuestion = {
       type,
       owner: req.user._id,
       questions: [
         {
-          question,
+          quesUrl,
           options:opt,
          
           
@@ -63,7 +68,7 @@ const quesUpload = asyncHandler(async (req, res) => {
 
   // Add new question to existing type
   typeOfQuestion.questions.push({
-    question,
+    quesUrl,
     options:opt,
     correct,
   });
@@ -75,6 +80,50 @@ const quesUpload = asyncHandler(async (req, res) => {
     "-questions.correct"
   );
   return res.status(200).json(new ApiResponse(200, que, "Question added"));
-});
+ });
+const randomques=asyncHandler(async(req,res)=>{
+  const {type}=req.body
+if(!type){
+  throw new ApiError(400, "Please provide type of question");
 
-export { quesUpload };
+}
+const typeOfQuestion=await Questions.findOne({$and:[{type},{owner:req.user?._id}]})
+if(!typeOfQuestion){
+  throw new ApiError(400, "No questions of this type exist");}
+  const randomques=typeOfQuestion.questions[Math.floor(Math.random()*typeOfQuestion.questions.length)]
+  if(!randomques){
+    throw new ApiError(400,"Error in finding random question")
+  }
+  const { correct, ...questionWithoutCorrect } = randomques._doc;
+  return res.status(200)
+  .json(new ApiResponse(200, questionWithoutCorrect, "Random question found"));
+  
+})
+const correctans=asyncHandler(async(req,res)=>{
+  const{type,question,selectedOption}=req.body;
+  if(!type){
+    throw new ApiError(400, "Please provide type of question");}
+    if(!selectedOption){
+      throw new ApiError(400, "Please select an option");
+    }
+    if(!question){
+      throw new ApiError(400, "Please provide question");
+    }
+    const typeOfQuestion=await Questions.findOne({$and:[{type},{owner:req.user?._id}]})
+    if(!typeOfQuestion){
+      throw new ApiError(400, "No questions of this type exist");
+    }
+    console.log(typeOfQuestion.questions)
+    const questionOfThisType=await typeOfQuestion.questions.find(q => q.question === question)
+    if(!questionOfThisType){
+      throw new ApiError(400, "No question of this type exist");
+    }
+    const correctans=questionOfThisType.correct
+    if(correctans===selectedOption){ 
+      return res.status(200).json(new ApiResponse(200,true,"Correct ans"))}
+      else{
+        return res.status(200).json(new ApiResponse(200,false,"Incorrect ans"))
+      }
+
+})
+export { quesUpload,randomques,correctans};
